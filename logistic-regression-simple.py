f8a4b05001be8d722599d27714c9af514088a7c4
fdiengott <freddy.diengott@proton.me>
2023-12-04 23:19:06 -0500

    create gradient descent for a logistic regression classification problem

--- logistic-regression-simple.py
+++ logistic-regression-simple.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy, math
 
 # outputs a scalar between 0 and 1
 def sigmoid(x):
@@ -13,14 +14,66 @@ def compute_cost(x, y, w, b):
     for i in range(m):
         z_i = np.dot(x[i], w) + b
         f_wb = sigmoid(z_i)
-        cost += y[i] * np.log(f_wb) + (1 - y[i]) * np.log(1 - f_wb)
+        cost -= y[i] * np.log(f_wb) - (1 - y[i]) * np.log(1 - f_wb)
 
-    cost /= -m
+    cost /= m
     return cost
 
+def normalize(x):
+    mu = np.mean(x, axis=0)
+    sigma = np.std(x, axis=0)
+
+    return (x - mu) / sigma
+
+
+def compute_gradient(x, y, w, b):
+    m,n = x.shape
+    derivative_w = np.zeros(n)
+    derivative_b = 0.
+
+    for i in range(m):
+        f_wb = sigmoid(np.dot(x[i],w) + b)
+        error = f_wb - y[i]
+        for j in range(n):
+            derivative_w += error * x[i, j]
+        derivative_b += error
+
+    derivative_w /= m
+    derivative_b /= m
+
+    return derivative_w, derivative_b
+
+
+
+def gradient_descent(x,y,w_init,b_init,alpha,num_iterations):
+    w = copy.deepcopy(w_init)
+    b = b_init
+
+    cost_history = []
+
+    for i in range(num_iterations):
+        derivative_w, derivative_b = compute_gradient(x,y,w,b)
+        w = w - alpha * derivative_w
+        w = w - alpha * derivative_b
+
+        if i % math.ceil(num_iterations / 10) == 0:
+            cost = compute_cost(x,y,w,b)
+            cost_history.append(cost)
+            print(f'cost: {cost:.2e}')
+
+    return w, b, cost_history
 
 x_train = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
 y_train = np.array([0, 0, 0, 1, 1, 1])
-w_tmp = np.array([1,1])
-b_tmp = -3
-print(compute_cost(x_train, y_train, w_tmp, b_tmp))
+num_examples = x_train.shape[0]
+
+w_temp = np.zeros_like(num_examples)
+b_temp = 0
+
+x_norm = normalize(x_train)
+
+print(f"initial cost: {compute_cost(x_norm, y_train, w_temp, b_temp)}")
+
+w_final, b_final = gradient_descent(x_norm, y_train, w_temp, b_temp, 0.1, 1000)
+
+print(w_final, b_final)
