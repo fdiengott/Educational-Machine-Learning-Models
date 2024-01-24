f98df38ec03b7ae5488f2230f37a03b2e0d4e123
fdiengott <freddy.diengott@proton.me>
2024-01-24 01:34:43 -0500

    add prompt template

--- llama.py
+++ llama.py
@@ -9,4 +9,10 @@ api_key = os.environ.get("LAMINI_API_KEY")
 lamini.api_key = os.environ.get("LAMINI_API_KEY")
 
 llm = lamini.LlamaV2Runner()
-print(llm("How are you?"))
+
+system_prompt="You are an assistant."
+instruction = "How are you?"
+
+prompt = f"<s>[INST] <<SYS>>\n{system_prompt}\n<</SYS>>\n\n{instruction}[/INST]"
+
+print(llm(prompt))
